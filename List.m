ae5e0d9a2130c04e09ad036a77c31080a2d453f6
Zackery Leman <zleman1593@gmail.com>
2014-04-28 20:29:10 -0400

    Update List.m

--- List.m
+++ List.m
@@ -1,12 +1,6 @@
-//
-//  List.m
-//
-//
-//  Created by Eric Chown on 4/4/13.
-// Modified by Zackery Leman on 11/20/13
-//  Copyright (c) 2013 edu.bowdoin.cs210.chown. All rights reserved.
+
 //Sets up the list data structure 
-//The only change from what you provided with us was forcing the data to be a NSMutableString
+
 
 #import "List.h"
 
