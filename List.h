6454a320ce6b120e54b5c8e1859109d90a7c3127
Zackery Leman <zleman1593@gmail.com>
2014-04-28 20:29:42 -0400

    Update List.h

--- List.h
+++ List.h
@@ -1,12 +1,6 @@
-//
-//  List.h
-//
-//
-//  Created by Eric Chown on 4/4/13.
-// Modified by Zackery Leman on 11/20/13
-//  Copyright (c) 2013 edu.bowdoin.cs210.chown. All rights reserved.
+
 //Sets up the list data structure
-//The only change from what you provided with us was forcing the data to be a CGPoint
+
 
 
 #import <Foundation/Foundation.h>
