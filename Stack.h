89eba10c3e90d6038e917d6f980ce2af48ed8904
Zackery Leman <zleman1593@gmail.com>
2014-04-28 20:30:17 -0400

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -1,10 +1,3 @@
-//
-//  Stack.h
-//  Cannibals
-//
-//  Created by Eric Chown on 4/9/13.
-//  Copyright (c) 2013 edu.bowdoin.cs210.chown. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 #import "List.h"
